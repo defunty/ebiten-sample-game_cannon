9189bd155718cdc22233782f39510ba2faad257d
defunty <tkqrt4131@gmail.com>
2021-09-26 17:15:32 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"fmt" // for debug
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"math"
+
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+const (
+	screenWidth  = 320
+	screenHeight = 240
+
+	frameOX     = 0  // cannon_spriteの関連値
+	frameOY     = 0  // cannon_spriteの関連値
+	frameWidth  = 26 // cannon_spriteの関連値
+	frameHeight = 26 // cannon_spriteの関連値
+	frameNum    = 11 // cannon_spriteの関連値
+)
+
+var (
+	cannonImage     *ebiten.Image
+	cannonBallImage *ebiten.Image
+)
+
+type mouseClick struct {
+	originX, originY int
+	currX, currY     int
+	duration         int
+}
+
+// for debug
+func OutputLog(s string) {
+	fmt.Println(s)
+	// log.Fatal(err)
+}
+
+func init() {
+	var err error
+	cannonImage, _, err = ebitenutil.NewImageFromFile("src/dist/image/sprite_cannon.png")
+	cannonBallImage, _, err = ebitenutil.NewImageFromFile("src/dist/image/cannonball.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Game struct {
+	count int
+
+	// mousePosition
+	mx, my int
+
+	// CannonBall's position
+	cbx int
+	cby int
+
+	// sound effect
+	// firePlayer   *audio.Player
+	// hitPlayer   *audio.Player
+	// criticalHitPlayer   *audio.Player
+}
+
+func calculateDegree(x0, y0, x1, y1 int) int {
+	//var tan float64 = float64(y1-y0) / float64(x1-x0)
+	//sita := math.Atan(tan)
+	radian := math.Atan2(float64(y1-y0), float64(x1-x0))
+	var degree int = int(math.Round(radian*180/math.Pi)) + 180 // 0 ~ 360（中心の左位置から時計回りで増加）
+
+	return degree
+}
+
+func (g *Game) drawCannon(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(-float64(frameWidth)/2, -float64(frameHeight)/2)
+	op.GeoM.Translate(screenWidth/2, screenHeight/2)
+	// i := (g.count / 5) % frameNum
+	degree := calculateDegree(screenWidth/2, screenHeight/2, g.mx, g.my)
+	i := degree
+	sx, sy := frameOX+i*frameWidth, frameOY
+	screen.DrawImage(cannonImage.SubImage(image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)).(*ebiten.Image), op)
+}
+
+func (g *Game) drawCannonBall(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(-float64(frameWidth)/2, -float64(frameHeight)/2)
+	op.GeoM.Translate(screenWidth/2, screenHeight/2)
+	// i := (g.count / 5) % frameNum
+	screen.DrawImage(cannonBallImage, op)
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	g.drawCannon(screen)
+	g.drawCannonBall(screen)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+// error型を返す
+func (g *Game) Update() error {
+	g.count++
+	g.mx, g.my = ebiten.CursorPosition()
+
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+		fmt.Println("hassya")
+
+	}
+
+	// What touches have just ended?
+	// for id, t := range g.touches {
+	// 	if inpututil.IsTouchJustReleased(id) {
+	// 		if g.pinch != nil && (id == g.pinch.id1 || id == g.pinch.id2) {
+	// 			g.pinch = nil
+	// 		}
+	// 		if g.pan != nil && id == g.pan.id {
+	// 			g.pan = nil
+	// 		}
+
+	// 		// If this one has not been touched long (30 frames can be assumed
+	// 		// to be 500ms), or moved far, then it's a tap.
+	// 		diff := distance(t.originX, t.originY, t.currX, t.currY)
+	// 		if !t.wasPinch && !t.isPan && (t.duration <= 30 || diff < 2) {
+	// 			g.taps = append(g.taps, tap{
+	// 				X: t.currX,
+	// 				Y: t.currY,
+	// 			})
+	// 		}
+
+	// 		delete(g.touches, id)
+	// 	}
+	// }
+	//
+	return nil
+}
+
+func main() {
+	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+	ebiten.SetWindowTitle("Render Image")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
